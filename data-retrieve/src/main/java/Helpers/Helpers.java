18d44aa0f3e7ebd3c5d6e54ab1a33e49d9b2c476
Grivel17 <wojciechowski.tomasz@gmail.com>
2023-02-17 18:01:50 +0100

    AP-01-data-retriev-service-refactor

---
+++ data-retrieve/src/main/java/Helpers/Helpers.java
@@ -0,0 +1,18 @@
+package Helpers;
+
+import dto.CompetitorDTO;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Helpers {
+
+    private static final int CURRENT_YEAR_AS_INT = LocalDate.now().getYear();
+
+    public Helpers() {
+    }
+
+    public static void ageToYearOfBirth(List<CompetitorDTO> competitorDTOList) {
+        competitorDTOList.stream().forEach(c -> c.setAge(CURRENT_YEAR_AS_INT - c.getAge()));
+    }
+}
---
+++ data-retrieve/src/main/java/services/DataStoreService.java
@@ -0,0 +1,30 @@
+package services;
+
+import domain.Competitor;
+import dto.CompetitorDTO;
+import repository.CompetitorRepository;
+
+import java.util.List;
+
+public class DataStoreService {
+    private final CompetitorRepository competitorRepository;
+
+    public DataStoreService(CompetitorRepository competitorRepository) {
+        this.competitorRepository = competitorRepository;
+    }
+
+    public void collectAllCompetitors(List<CompetitorDTO> competitorDTOList){
+        competitorDTOList.stream()
+                .forEach(competitorDTO -> competitorRepository.save(getCompetitor(competitorDTO)));
+    }
+
+    private static Competitor getCompetitor(CompetitorDTO competitorDTO) {
+        return new Competitor(
+                competitorDTO.getName(),
+                competitorDTO.getSurName(),
+                competitorDTO.getAge(),
+                competitorDTO.getClub(),
+                competitorDTO.getCategory()
+        );
+    }
+}
