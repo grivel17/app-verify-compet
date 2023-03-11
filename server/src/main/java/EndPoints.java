9064f437f51ade9f511eb1367731e7f5c14a6192
Grivel17 <wojciechowski.tomasz@gmail.com>
2023-03-11 14:49:35 +0100

    AP-01-data-retriever-service-server-first-end-point

--- server/src/main/java/EndPoints.java
+++ server/src/main/java/EndPoints.java
@@ -1,2 +1,22 @@
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("endpoints")
 public class EndPoints {
+
+    //ta klasa musi mieć trafić do metody register, żeby serwer ją obsłużył
+
+    // todo to do ogarnięcia https://www.oracle.com/webfolder/technetwork/tutorials/obe/java/griz_jersey_intro/Grizzly-Jersey-Intro.html
+
+    public EndPoints() {
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTest() {
+        return "Method get works fine";
+    }
+
 }
--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -9,19 +9,28 @@ import java.net.URI;
 public class Server {
 
     // base url
-    public static final String BASE_URL = "http://localhost:8181";
+    public static final String BASE_URL = "http://localhost:8181/";
+
+    //move to properties file
     private static final String dbFile = "./competitors.db";
 
     // run server method
 
     public static HttpServer startServer() {
         CompetitorRepository competitorRepository = new CompetitorJDBCRepository(dbFile);
-        final ResourceConfig config = new ResourceConfig().register(competitorRepository);
+        // todo - jedno z drugim przekazać po konstruktorze
+        // todo - endpoitns lecą do konfigu ale zawieraja pole thisowane przez konstruktor
+        // todo - które to pole daje dostep do interface i jego method
+
+        EndPoints endPoints = new EndPoints();
+
+        final ResourceConfig config = new ResourceConfig().register(endPoints);
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URL), config);
     }
 
     public static void main(String[] args) {
         startServer();
+
     }
 
 }
