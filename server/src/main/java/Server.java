0aa448e5fb1f0b5699b02409bc08f793b39f3521
Grivel17 <wojciechowski.tomasz@gmail.com>
2023-03-11 14:26:45 +0100

    AP-01-data-retriever-service-server-added

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -1,2 +1,27 @@
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import repository.CompetitorJDBCRepository;
+import repository.CompetitorRepository;
+
+import java.net.URI;
+
 public class Server {
+
+    // base url
+    public static final String BASE_URL = "http://localhost:8181";
+    private static final String dbFile = "./competitors.db";
+
+    // run server method
+
+    public static HttpServer startServer() {
+        CompetitorRepository competitorRepository = new CompetitorJDBCRepository(dbFile);
+        final ResourceConfig config = new ResourceConfig().register(competitorRepository);
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URL), config);
+    }
+
+    public static void main(String[] args) {
+        startServer();
+    }
+
 }
